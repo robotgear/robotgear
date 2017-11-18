d11572c827ac0b62280c4acee13ff2b6ecb4be40
bkeeneykid <bkeeneykid@me.com>
2017-11-17 19:39:37 -0600

    Start on creating models

--- market/models.py
+++ market/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class Post(models.Model):
+	title = models.CharField(max_length=100)
+	desc = models.TextField()
+	creator = models.ForeignKey(settings.AUTH_USER_MODEL,
+	                            on_delete=models.CASCADE)
+
