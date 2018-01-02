7c61daef147c0d75583283570fae462f55e5f5b3
bkeeneykid <bkeeneykid@me.com>
2018-01-01 21:35:13 -0600

    Login System Fuctional

--- market/views.py
+++ market/views.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.shortcuts import redirect
 
 # Create your views here.
 
 def index(request):
 	return render(request, 'index.html')
+
+def loginView(request):
+	if request.user.is_authenticated:
+		return redirect(index)
+	if request.method == "POST":
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			return redirect(index)
+		else:
+			messages.error(request, "Inorrect login details.")
+			return render(request,'login.html')
+	else:
+		return render(request, 'login.html')
+
+def logoutView(request):
+	if request.user.is_authenticated:
+		logout(request)
+		return redirect(index)
+	else:
+		return redirect(loginView)
