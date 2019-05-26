a06973a3a1ffb8e51025c8a7157088971d0ba2de
bkeeneykid <bkeeneykid@me.com>
2019-05-26 23:20:30 +0200

    Swapped to semantic UI, add requirements.txt

--- static/js/init.js
+++
@@ -1,9 +0,0 @@
-(function($){
-  $(function(){
-
-      $('.dropdown-trigger').dropdown();
-    $('.sidenav').sidenav();
-    $('.tooltipped').tooltip();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
