766185ced758b5ea910d927373450b4cd92fa7bc
bkeeneykid <bkeeneykid@me.com>
2019-05-27 13:49:40 +0200

    Switch to PostgreSQL

--- robotgear/settings.py
+++ robotgear/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'robotgear.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'robotgear',
+        'USER': 'devin',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
